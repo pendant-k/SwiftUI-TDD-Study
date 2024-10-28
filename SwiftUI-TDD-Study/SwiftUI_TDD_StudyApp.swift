6a001e8aaca9c9bf3fb3090faf811c54025c8c94
Donghan Kim <54224868+pendant-k@users.noreply.github.com>
2024-10-28 21:18:53 +0900

    Initial Commit

---
+++ SwiftUI-TDD-Study/SwiftUI_TDD_StudyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_TDD_StudyApp.swift
+//  SwiftUI-TDD-Study
+//
+//  Created by Donghan Kim on 10/28/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_TDD_StudyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
